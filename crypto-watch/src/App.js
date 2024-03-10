488dfb68251aa561787f8810c373a0502466526f
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2024-03-10 12:50:27 -0500

    added .env, gitignore and api key to project

--- crypto-watch/src/App.js
+++ crypto-watch/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [coins, setCoins]= useState([]);
+
+  useEffect(()=>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets').then((response) => {
+      setCoins(response.data)
+      console.log(response.data)
+    }).catch((error) => {
+        console.log(error)
+    })
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    </>
   );
 }
 
